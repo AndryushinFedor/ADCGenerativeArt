5af93866dd4479ac01d114d0d3d0e63fba0ff91a
Fedor <andryushin.fedor@gmail.com>
2022-05-27 16:02:15 +0300

    upd

---
+++ app/javascript/packs/prototype_43.js
@@ -0,0 +1,39 @@
+import p5 from 'p5'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementsByClassName('prototype_43')[0]
+  const frame = document.createElement('div')
+  frame.classList.add('frame')
+  frame.id = 'frame'
+  container.appendChild(frame)
+
+    let s = 2;
+    let y = 0;
+    let a = 2;
+
+  let sketch = (p) => {
+    p.setup = () => {
+      let canvas = p.createCanvas(700, 700, p.WEBGL)
+      canvas.parent('frame')
+
+
+      p.frameRate(60)
+      p.background(300)
+    }
+
+    p.draw = () => {
+      p.fill(250);
+      p.rotateY(y)
+      p.scale(s);
+      p.sphere()
+      
+
+      y += 0.01
+      s = p.cos(a) * 2;
+
+
+    }
+  }
+
+  let myp5 = new p5(sketch)
+})
